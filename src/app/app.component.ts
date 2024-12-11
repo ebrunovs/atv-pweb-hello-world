162d4037e60cb5c7983beaec845dc470db7bc964
bruno <brunovs.eneas@gmail.com>
2024-12-11 01:05:39 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primeiros-passos';
+  aluno = 'Bruno Vinicius de Araujo Eneas';
 }
